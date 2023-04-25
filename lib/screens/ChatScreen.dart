9847e8ec36d347af29a8e6258a6053933ce1ea90
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-04-25 20:18:29 +0530

    init

--- lib/screens/ChatScreen.dart
+++ lib/screens/ChatScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 import '../common/chat.dart';
@@ -7,7 +9,10 @@ class ChatScreen extends StatefulWidget {
   bool isShipped = false;
   bool isClosed = false;
 
-  ChatScreen({required this.isReview,required this.isShipped, required this.isClosed});
+  ChatScreen(
+      {required this.isReview,
+      required this.isShipped,
+      required this.isClosed});
 
   @override
   _ChatScreenState createState() => _ChatScreenState();
@@ -17,122 +22,110 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children:  [
-          const ChatMessage(
-            id: 'chat-message',
-            content: Text(
-              'Hi Jason! How are you?',
-              style: TextStyle(color: Colors.black),
-            ),
-            received: true,
-            chatBubbleColor: Colors.white,
+        body: ListView(
+      children: [
+        const ChatMessage(
+          id: 'chat-message',
+          content: Text(
+            'Hi Jason! How are you?',
+            style: TextStyle(color: Colors.black),
           ),
-          const ChatMessage(
-            id: 'chat-message',
-            content: Text(
-              'I`m good, how about you?',
-              style: TextStyle(color: Colors.white),
-            ),
-            received: false,
-            chatBubbleColor: Colors.purple,
+          received: true,
+          chatBubbleColor: Colors.white,
+        ),
+        const ChatMessage(
+          id: 'chat-message',
+          content: Text(
+            'I`m good, how about you?',
+            style: TextStyle(color: Colors.white),
           ),
-          const ChatMessage(
-            id: 'chat-message',
-            content: Text(
-              'How is the work going?',
-              style: TextStyle(color: Colors.black),
-            ),
-            received: true,
-            chatBubbleColor: Colors.white,
+          received: false,
+          chatBubbleColor: Colors.purple,
+        ),
+        const ChatMessage(
+          id: 'chat-message',
+          content: Text(
+            'How is the work going?',
+            style: TextStyle(color: Colors.black),
           ),
-          widget.isReview
-          ? const ChatMessage(
-            id: 'chat-message',
-            content: Text(
-              'I`ve just reviewed the project.',
-              style: TextStyle(color: Colors.white),
-            ),
-            received: false,
-            chatBubbleColor: Colors.purple,
-          )
-          : const SizedBox(),
+          received: true,
+          chatBubbleColor: Colors.white,
+        ),
 
-           widget.isShipped
-          ? const ChatMessage(
-            id: 'chat-message',
-            content: Text(
-              'I`ve just shipped the project.',
-              style: TextStyle(color: Colors.white),
-            ),
-            received: false,
-            chatBubbleColor: Colors.purple,
-          )
-          : const SizedBox(),
-
-           widget.isClosed
-          ? const ChatMessage(
-            id: 'chat-message',
-            content: Text(
-              'I`ve just closed the project.',
-              style: TextStyle(color: Colors.white),
-            ),
-            received: false,
-            chatBubbleColor: Colors.purple,
-          )
-          : const ChatMessage(
-            id: 'chat-message',
-            content: Text(
-              'I`ve just reviewed the project.',
-              style: TextStyle(color: Colors.white),
-            ),
-            received: false,
-            chatBubbleColor: Colors.purple,
+        widget.isClosed
+            ? const ChatMessage(
+                id: 'chat-message',
+                content: Text(
+                  'I`ve just closed the project.',
+                  style: TextStyle(color: Colors.white),
+                ),
+                received: false,
+                chatBubbleColor: Colors.purple,
+              )
+            : widget.isShipped
+                ? const ChatMessage(
+                    id: 'chat-message',
+                    content: Text(
+                      'I`ve just shipped the project.',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    received: false,
+                    chatBubbleColor: Colors.purple,
+                  )
+                : widget.isReview
+                    ? const ChatMessage(
+                        id: 'chat-message',
+                        content: Text(
+                          'I`ve just reviewed the project.',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        received: false,
+                        chatBubbleColor: Colors.purple,
+                      )
+                    : const SizedBox(),
+        // ChatMessage(
+        //   id: 'chat-message',
+        //   content: widget.isReview
+        //       ? Text(
+        //           'I`ve just reviewed the project.',
+        //           style: TextStyle(color: Colors.white),
+        //         )
+        //       : widget.isShipped
+        //           ? Text(
+        //               'I`ve just shipped the project.',
+        //               style: TextStyle(color: Colors.white),
+        //             )
+        //           : widget.isClosed
+        //               ? Text(
+        //                   'I`ve just closed the project.',
+        //                   style: TextStyle(color: Colors.white),
+        //                 )
+        //               : Text(
+        //                   'I`ve just reviewed the project.',
+        //                   style: TextStyle(color: Colors.white),
+        //                 ),
+        //   received: false,
+        //   chatBubbleColor: Colors.purple,
+        // ),
+        ChatMessage(
+          id: 'chat-message',
+          content: Text(
+            'Great! Keep it up!',
+            style: TextStyle(color: Colors.black),
           ),
-          // ChatMessage(
-          //   id: 'chat-message',
-          //   content: widget.isReview
-          //       ? Text(
-          //           'I`ve just reviewed the project.',
-          //           style: TextStyle(color: Colors.white),
-          //         )
-          //       : widget.isShipped
-          //           ? Text(
-          //               'I`ve just shipped the project.',
-          //               style: TextStyle(color: Colors.white),
-          //             )
-          //           : widget.isClosed
-          //               ? Text(
-          //                   'I`ve just closed the project.',
-          //                   style: TextStyle(color: Colors.white),
-          //                 )
-          //               : Text(
-          //                   'I`ve just reviewed the project.',
-          //                   style: TextStyle(color: Colors.white),
-          //                 ),
-          //   received: false,
-          //   chatBubbleColor: Colors.purple,
-          // ),
-          ChatMessage(
-            id: 'chat-message',
-            content: Text(
-              'Great! Keep it up!',
-              style: TextStyle(color: Colors.black),
-            ),
-            received: true,
-            chatBubbleColor: Colors.white,
+          received: true,
+          chatBubbleColor: Colors.white,
+        ),
+        ChatMessage(
+          id: 'chat-message',
+          content: Text(
+            'Thanks!',
+            style: TextStyle(color: Colors.white),
           ),
-          ChatMessage(
-            id: 'chat-message',
-            content: Text(
-              'Thanks!',
-              style: TextStyle(color: Colors.white),
-            ),
-            received: false,
-            chatBubbleColor: Colors.purple,
-          ),
-        ],
-      )
-    );
+          received: false,
+          chatBubbleColor: Colors.purple,
+        ),
+      ],
+    ));
   }
 }
